7aa78f423f085122a8bd945e74be93beb904de92
EdsonArmando <edsonguix@gmail.com>
2021-04-14 13:12:44 -0500

    Funciona el ordenamiento rapido y burbuja

--- Instrucciones/Asignacion.cs
+++ Instrucciones/Asignacion.cs
@@ -41,7 +41,8 @@ namespace Proyecto1_Compi2.Instrucciones
                 //Arreglo de una dimension
                 if (posX != null && posY == null && posZ == null)
                 {
-                    instance.agregarComentario("Iniciando Proceso de Asignacion de arreglo");
+                    String[] posInicialX = sim.posicion_X;
+                    instance.agregarComentario("Iniciando Proceso de Asignacion de arreglo Global");
                     //Posicion en la que se va a insertar en el arreglo
                     Retornar posx = posX.Compilar(ent);
                     //Valor en la poscion x del arreglo
@@ -49,24 +50,32 @@ namespace Proyecto1_Compi2.Instrucciones
                     //Obtener el valor del Stack
                     String tempInicio = instance.newTemporal();instance.freeTemp(tempInicio);
                     instance.addGetStack(tempInicio,sim.posicion);
+                    //Salto una posicion
+                    String tempInicioArray = instance.newTemporal(); instance.freeTemp(tempInicio);
+                    instance.addExpression(tempInicioArray, tempInicio, "1", "+");
+                    //Hacer resta entre la posicion iniciall del arreglo
+                    String tempResta = instance.newTemporal(); instance.freeTemp(tempResta);
+                    instance.addExpression(tempResta, posx.getValue(),posInicialX[0], "-");
                     //Temporal donde se Guardar posicion a insertar en el Heap
                     String tempHeap = instance.newTemporal();instance.freeTemp(tempHeap);
-                    instance.addExpression(tempHeap,tempInicio,posx.getValue(),"+");
+                    instance.addExpression(tempHeap, tempInicioArray, tempResta, "+");
                     //String tempHeap2 = instance.newTemporal(); instance.freeTemp(tempHeap2);
                     //instance.addExpression(tempHeap2, tempHeap, "1", "+");
                     //Ingreso el valor en el Heap
                     instance.addSetHeap(tempHeap, val.getValue());
+                    return null;
                 }
                 //Arreglo de dos dimensiones
                 else if (posX != null && posY != null && posZ == null)
                 {
 
-
+                    return null;
                 }
                 //Arreglo de 3 dimensiones
                 else if (posX != null && posY != null && posZ != null)
                 {
 
+                    return null;
                 }
                 else {
                     if (value.tipo == Simbolo.EnumTipoDato.BOOLEAN)
@@ -78,14 +87,57 @@ namespace Proyecto1_Compi2.Instrucciones
                         instance.addLabel(value.falseLabel);
                         instance.addSetStack(sim.posicion.ToString(), "0");
                         instance.addLabel(templabel);
+                        return null;
                     }
                     else {
                         instance.addSetStack(sim.posicion.ToString(), value.getValue());
+                        return null;
                     }                    
                 }                
             }
             else {
-                if (value.tipo == Simbolo.EnumTipoDato.BOOLEAN)
+                //Arreglo de una dimension
+                if (posX != null && posY == null && posZ == null)
+                {
+                    instance.agregarComentario("Iniciando Proceso de Asignacion de arreglo no Global");
+                    String temp = instance.newTemporal();
+                    instance.freeTemp(temp);
+                    instance.addExpression(temp, "p", sim.posicion.ToString(), "+");
+                    String[] posInicialX = sim.posicion_X;    
+                    //Posicion en la que se va a insertar en el arreglo
+                    Retornar posx = posX.Compilar(ent);
+                    //Valor en la poscion x del arreglo
+                    Retornar val = valor.Compilar(ent);
+                    //Obtener el valor del Stack
+                    String tempInicio = instance.newTemporal(); instance.freeTemp(tempInicio);
+                    instance.addGetStack(tempInicio, sim.posicion);
+                    //Salto una posicion
+                    String tempInicioArray = instance.newTemporal(); instance.freeTemp(tempInicio);
+                    instance.addExpression(tempInicioArray, tempInicio, "1", "+");
+                    //Hacer resta entre la posicion iniciall del arreglo
+                    String tempResta = instance.newTemporal(); instance.freeTemp(tempResta);
+                    instance.addExpression(tempResta, posx.getValue(), posInicialX[0], "-");
+                    //Temporal donde se Guardar posicion a insertar en el Heap
+                    String tempHeap = instance.newTemporal(); instance.freeTemp(tempHeap);
+                    instance.addExpression(tempHeap, tempInicioArray, tempResta, "+");
+                    //String tempHeap2 = instance.newTemporal(); instance.freeTemp(tempHeap2);
+                    //instance.addExpression(tempHeap2, tempHeap, "1", "+");
+                    //Ingreso el valor en el Heap
+                    instance.addSetHeap(tempHeap, val.getValue());
+                    return null;
+                }
+                //Arreglo de dos dimensiones
+                else if (posX != null && posY != null && posZ == null)
+                {
+
+                    return null;
+                }
+                //Arreglo de 3 dimensiones
+                else if (posX != null && posY != null && posZ != null)
+                {
+
+                    return null;
+                }else if (value.tipo == Simbolo.EnumTipoDato.BOOLEAN)
                 {
 
                     String templabel = instance.newLabel();
@@ -109,7 +161,6 @@ namespace Proyecto1_Compi2.Instrucciones
                 }               
                 return null;
             }            
-            return null;
         }       
     }
 }
